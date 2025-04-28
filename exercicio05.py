5f4f80de85c18b768a1b8b51be693c9ae5d658d2
Cauã Souza <cauasouza3112@gmail.com>
2025-04-28 16:26:52 -0300

    Exercicio com multiplicador

---
+++ exercicio05.py
@@ -0,0 +1,11 @@
+a=[""]*5
+m=[""]*5
+x=0
+
+for i in range(len(a)):
+    a[1]=int(input("digite um numero"))
+
+x=int(input("Digite o multiplicador: "))
+for y in range(len(m)):
+    m[y]=x*a[y]
+print(a)

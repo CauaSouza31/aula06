8a6e5574e34d049674bf34108666ffd6dce1af15
Cauã Souza <cauasouza3112@gmail.com>
2025-04-28 14:51:07 -0300

    igual ao outro

---
+++ exercicio02.py
@@ -0,0 +1,5 @@
+arrayAlunos=['','','','','']
+for x in range(len(arrayAlunos)):
+   arrayAlunos[x]=input("Digite seu nome: ")
+for y in range(len(arrayAlunos)):
+    print(f"{arrayAlunos[y]} está na posição {y}")
